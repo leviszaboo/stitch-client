73312ba935ef2c253ecba0420f71e944a54f685d
Levi <leviszaboo@icloud.com>
2023-10-19 01:02:23 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  function onSubmit() {
+    return axios.post('http://localhost:3001/test')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <input type="text" />
+          <input type="file" />
+          <button onClick={onSubmit}>submit</button>
+        </form>
       </header>
     </div>
   );
